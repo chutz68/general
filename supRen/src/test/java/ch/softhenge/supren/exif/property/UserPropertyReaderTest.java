47044c4f96b0c396e521898b1c68a1c9f86af48f
Werner <Werner@RuRos>
2015-01-06 22:12:50 +0100

    nikon added

--- supRen/src/test/java/ch/softhenge/supren/exif/property/UserPropertyReaderTest.java
+++ supRen/src/test/java/ch/softhenge/supren/exif/property/UserPropertyReaderTest.java
@@ -39,11 +39,11 @@ public class UserPropertyReaderTest {
 				checkPropertyEntry(expect, propertyEntry);
 				break;
 		    case CameraModel:
-		    	expect = 31;
+		    	expect = 32;
 				checkPropertyEntry(expect, propertyEntry);
 				break;
 			case CameraModel4ch:
-				expect = 31;
+				expect = 32;
 				checkPropertyEntry(expect, propertyEntry);
 				break;
 			case OutfilePattern:
