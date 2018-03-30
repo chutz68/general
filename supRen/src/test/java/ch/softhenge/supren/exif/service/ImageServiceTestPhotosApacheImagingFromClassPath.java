42a215033305bee872619c1598a089eed683f170
THARSIS\Werni <werner.rodel@gmail.com>
2018-03-30 23:56:02 +0200

    new test classes for metadataextract and apacheimaging

---
+++ supRen/src/test/java/ch/softhenge/supren/exif/service/ImageServiceTestPhotosApacheImagingFromClassPath.java
@@ -0,0 +1,134 @@
+package ch.softhenge.supren.exif.service;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.softhenge.supren.exif.entity.FilePattern;
+import ch.softhenge.supren.exif.entity.ImageFile;
+
+/**
+ * Test Photos from Class Path
+ * 
+ * @author werni
+ *
+ */
+public class ImageServiceTestPhotosApacheImagingFromClassPath {
+
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+
+	private ImageService imageService;
+
+	@Before
+	public void setUp() throws Exception {
+		String fileURL = this.getClass().getClassLoader().getResource("imgfiles").getPath();
+		imageService = new ImageService("ruro.properties", fileURL, false, new ExifServiceApacheImaging());
+		LOGGER.setLevel(Level.FINE);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+		LOGGER.addHandler(handler);
+	}
+
+	@Test
+	public void testListImageFilesToRename() {
+		imageService.createImageFilesMap(null, null);
+		imageService.resetImageFileList();
+		imageService.createImageFilesMap(null, null);
+		imageService.createImageFilesMap(null, null);
+		Map<FilePattern, Collection<ImageFile>> mapOfImageFiles = imageService.getMapOfImageFiles();
+		imageService.enrichImageFilesWithExifInfo(new StringBuffer());
+		int cntAll = 0;
+		int cntKnown = 0;
+		int cntUnkP = 0;
+		int cntOutP = 0;
+		for (Collection<ImageFile> imageFiles : mapOfImageFiles.values()) {
+			for (ImageFile imageFile : imageFiles) {
+				LOGGER.info(imageFile.getOriginalFileName());
+				cntAll++;
+				if (imageFile.isKnownCameraModel()) {
+					cntKnown++;
+				}
+				if (imageFile.getFilePattern().isOutPattern()) {
+					cntOutP++;
+				}
+				if (imageFile.getFilePattern().isUnknownPattern()) {
+					cntUnkP++;
+				}
+			}
+		}
+		assertThat("Count all is wrong", cntAll, CoreMatchers.is(6));
+		assertThat("Count Known is wrong", cntKnown, CoreMatchers.is(4));
+		assertThat("Count Unknown Pattern is wrong", cntUnkP, CoreMatchers.is(1));
+		assertThat("Count Outfile Pattern is wrong", cntOutP, CoreMatchers.is(3));
+	}
+
+	@Test
+	public void testMvCommand() {
+		imageService.createMvAndUndoCommands(null);
+		String mvCommand = imageService.getMvCommand();
+		checkMvCommand(mvCommand);
+		LOGGER.fine("mvCommands");
+		LOGGER.fine(mvCommand);
+	}
+
+	@Test
+	public void testMvUndoCommand() {
+		imageService.createMvAndUndoCommands(null);
+		String mvUndoCommand = imageService.getMvUndoCommand();
+		checkMvCommand(mvUndoCommand);
+		LOGGER.fine("mvUndoCommand");
+		LOGGER.fine(mvUndoCommand);
+	}
+
+	@Test
+	public void testMvAlreadyDone() {
+		imageService.createMvAndUndoCommands(null);
+		String mvAlreadyDone = imageService.getMvAlreadyDone();
+		assertThat(mvAlreadyDone, CoreMatchers.containsString("20091031_E400_2177.JPG"));
+
+		LOGGER.fine("mvAlreadyDone");
+		LOGGER.fine(mvAlreadyDone);
+	}
+	
+	@Test
+	public void testMvError() {
+		imageService.createMvAndUndoCommands(null);
+		String mvError = imageService.getMvError();
+		int countMatches = org.apache.commons.lang3.StringUtils.countMatches(mvError, "# ImageFile ");
+		assertThat("Number of error mv commands", countMatches, CoreMatchers.is(2));
+		assertThat(mvError, CoreMatchers.containsString("C6ZH_019.JPG"));
+		assertThat(mvError, CoreMatchers.containsString("Img_0001.jpg"));
+		assertThat(mvError, CoreMatchers.containsString("Filepattern is unknown"));
+		assertThat(mvError, CoreMatchers.containsString("Unknown Camera type"));
+
+		LOGGER.fine("mvError");
+		LOGGER.fine(mvError);
+	}
+	
+	@Test
+	public void testGetListOfUnknownImageFiles() {
+		Collection<ImageFile> imageFiles = imageService.getListOfUnknownImageFiles();
+		assertEquals(imageFiles.size(), 0);
+	}
+	
+	@Test
+	public void testCreateCsvSeperatedStringOfImageFiles() {
+		String csvText = imageService.createCsvSeperatedStringOfImageFiles(null);
+		LOGGER.info(csvText);
+	}
+	
+	private void checkMvCommand(String mvCommand) {
+		int countMatches = org.apache.commons.lang3.StringUtils.countMatches(mvCommand, "mv ");
+		assertThat("Number of mv commands " + mvCommand, countMatches, CoreMatchers.is(1));
+		assertThat(mvCommand, CoreMatchers.containsString("IMG_0652.jpg"));
+	}
+	
+}
---
+++ supRen/src/test/java/ch/softhenge/supren/exif/service/RenameNewPhotosApacheImageTest.java
@@ -0,0 +1,62 @@
+package ch.softhenge.supren.exif.service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RenameNewPhotosApacheImageTest {
+
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+	private final static long DAYS_BACK = 0;
+	private final static boolean FORCE_UNKNOWN_IMAGES = false;
+	//private final static String[] DIRECTORIES = { "D:\\", "C:\\" };
+	//private final static String[] DIRECTORIES = { "D:\\photos", "C:\\" };
+	//private final static String[] DIRECTORIES = { "D:\\photos" };
+	private final static String[] DIRECTORIES = { "D:\\phototest" };
+	
+	private ImageService imageService;
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd-HHmm");
+
+	@Before
+	public void setUp() throws Exception {
+		LOGGER.setLevel(Level.FINE);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+		LOGGER.addHandler(handler);
+	}
+	
+	@Test
+	public void testCreateCsvSeperatedStringOfImageFilesandMv() throws IOException {
+		File file = new File("mvCommand_" + dateFormat.format(new Date()));
+		FileWriter fw = new FileWriter(file);
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    bw = new BufferedWriter(fw);
+	    
+		File fileErr = new File("error_" + dateFormat.format(new Date()));
+		FileWriter fwErr = new FileWriter(fileErr);
+	    BufferedWriter bwErr = new BufferedWriter(fwErr);
+	    bwErr = new BufferedWriter(fwErr);
+
+	    for (String directory : DIRECTORIES) {
+			imageService = new ImageService("ruro.properties", directory, FORCE_UNKNOWN_IMAGES, new ExifServiceApacheImaging());
+			imageService.createMvAndUndoCommands(DAYS_BACK);
+			String mvCommand = imageService.getMvCommand();
+			bw.write(mvCommand);
+			String error = imageService.getMvError();
+			bwErr.write(error);			
+		}
+	    bw.close();	
+	    bwErr.close();	
+	}
+	
+}
