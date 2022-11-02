4b304de199948232c1a496420352f6d58496621b
werner <werner.rodel@gmail.com>
2022-11-02 23:20:16 +0100

    configurable API key

--- SolarlogReader/src/test/java/ch/softhenge/solarlog/weather/WeatherServiceTest.java
+++ SolarlogReader/src/test/java/ch/softhenge/solarlog/weather/WeatherServiceTest.java
@@ -1,15 +1,25 @@
 package ch.softhenge.solarlog.weather;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class WeatherServiceTest {
 
     @Autowired
+    private RestTemplate restTemplate;
     private WeatherService weatherService;
+
+    @BeforeEach
+    void beforeTest() {
+        this.weatherService = new WeatherService("Neuenhof", restTemplate);
+    }
+
 
     @Test
     public void testWeatherService() {
