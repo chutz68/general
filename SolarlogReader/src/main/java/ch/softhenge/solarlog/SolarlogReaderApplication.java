41960561159b5b1d686d1c1b45a1587140982ed1
werner <werner.rodel@gmail.com>
2022-11-04 23:48:43 +0100

    Add a commandline implementation

--- SolarlogReader/src/main/java/ch/softhenge/solarlog/SolarlogReaderApplication.java
+++ SolarlogReader/src/main/java/ch/softhenge/solarlog/SolarlogReaderApplication.java
@@ -1,9 +1,13 @@
 package ch.softhenge.solarlog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SolarlogReaderApplication {
@@ -15,6 +19,18 @@ public class SolarlogReaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolarlogReaderApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Spring boot beans");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 
 }
