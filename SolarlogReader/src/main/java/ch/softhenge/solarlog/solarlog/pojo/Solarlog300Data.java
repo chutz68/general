c0ed43689c517b04d77060e597476023a0c534c5
werner <werner.rodel@gmail.com>
2022-11-09 23:16:06 +0100

    Solarlog300 class contains of a map and ready all needed fields

--- SolarlogReader/src/main/java/ch/softhenge/solarlog/solarlog/pojo/Solarlog300Data.java
+++ SolarlogReader/src/main/java/ch/softhenge/solarlog/solarlog/pojo/Solarlog300Data.java
@@ -6,12 +6,13 @@ import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * This is an object that is created if the solarlog 300 is read
+ * This is an object that contains the solarlog 300 json data in HashMaps
  */
 public class Solarlog300Data implements Serializable {
 
@@ -19,15 +20,27 @@ public class Solarlog300Data implements Serializable {
      * This enum represents the json fields in the solarlog return value
      */
     public enum SOLARLOG300_REGISTER {
-        CREATEDDATE("createdDate", "100", LocalDateTime.class),
-        PACWRALL("pacWrAll", "101", Integer.class);
+        CREATEDDATE("100", LocalDateTime.class),
+        PACWRALL( "101", Integer.class),
+        PDCWRALL("102", Integer.class),
+        UACWRALL( "103", Integer.class),
+        UDCWRALL( "104", Integer.class),
+        EAC_DAYSUM_WRALL("105", Integer.class),
+        EAC_DAYSUM_YESTERDAY_WRALL( "106", Integer.class),
+        EAC_MONTHSUM_WRALL("107", Integer.class),
+        EAC_YEARSUM_WRALL( "108", Integer.class),
+        EAC_TOTAL_WRALL( "109", Integer.class),
+        PAC_CNT( "110", Integer.class),
+        EAC_DAYSUM_CNT( "111", Integer.class),
+        EAC_DAYSUM_YESTERDAY_CNT( "112", Integer.class),
+        EAC_MONTHSUM_CNT( "113", Integer.class),
+        EAC_YEARSUM_CNT("114", Integer.class),
+        EAC_TOTAL_CNT( "115", Integer.class);
 
-        private final String fieldName;
         private final String register;
         private final Class<?> fieldClass;
 
-        SOLARLOG300_REGISTER(String fieldName, String register, Class<?> fieldClass) {
-            this.fieldName = fieldName;
+        SOLARLOG300_REGISTER(String register, Class<?> fieldClass) {
             this.register = register;
             this.fieldClass = fieldClass;
         }
@@ -36,43 +49,58 @@ public class Solarlog300Data implements Serializable {
 
     private final static String SOLARLOG300_JSON_OBJID_1 = "801";
     private final static String SOLARLOG300_JSON_OBJID_2 = "170";
-
     private final static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm:ss");
 
-    private LocalDateTime createdDate;
-    private Integer pacWrAll;
+    private final Map<SOLARLOG300_REGISTER, Integer> solarlogIntegerMap;
+    private final Map<SOLARLOG300_REGISTER, LocalDateTime> solarlogDateMap;
 
 
+    /**
+     * The constructor creates a class containing all values of the Solarlog Json, storing them to HashMaps
+     *
+     * @param solarlogLogJsonString the JSON as String
+     */
     public Solarlog300Data(String solarlogLogJsonString) {
+        solarlogIntegerMap = new HashMap<>();
+        solarlogDateMap = new HashMap<>();
+
         JsonParser parser = new JsonParser();
         JsonElement jsonElement = parser.parse(solarlogLogJsonString);
         JsonObject jsonObj = jsonElement.getAsJsonObject();
         jsonObj = jsonObj.getAsJsonObject(Solarlog300Data.SOLARLOG300_JSON_OBJID_1);
         jsonObj = jsonObj.getAsJsonObject(Solarlog300Data.SOLARLOG300_JSON_OBJID_2);
 
-        Field[] fields = Solarlog300Data.class.getDeclaredFields();
         for (SOLARLOG300_REGISTER slr : SOLARLOG300_REGISTER.values()) {
-            JsonPrimitive primitive = jsonObj.get(slr.register).getAsJsonPrimitive();
-            try {
-                Field f = Solarlog300Data.class.getDeclaredField(slr.fieldName);
-                if (Integer.class.equals(slr.fieldClass)) {
-                    f.set(this, primitive.getAsInt());
-                }
-                if (LocalDateTime.class.equals(slr.fieldClass)) {
-                    String dateAsString = primitive.getAsString();
-                    f.set(this, LocalDateTime.parse(dateAsString, dtf));
-                }
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new RuntimeException(e);
+            JsonPrimitive jsonPrimitive = jsonObj.get(slr.register).getAsJsonPrimitive();
+            if (Integer.class.equals(slr.fieldClass)) {
+                solarlogIntegerMap.put(slr, jsonPrimitive.getAsInt());
+            }
+            if (LocalDateTime.class.equals(slr.fieldClass)) {
+                String dateAsString = jsonPrimitive.getAsString();
+                solarlogDateMap.put(slr, LocalDateTime.parse(dateAsString, dtf));
             }
         }
     }
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
+
+    /**
+     * returns the integer value of the requested key
+     *
+     * @param register the key that was requested
+     * @return the integer belonging to the key
+     */
+    public Integer getSolarlogIntegerField(SOLARLOG300_REGISTER register) {
+        return solarlogIntegerMap.get(register);
     }
 
-    public Integer getPacWrAll() {
-        return pacWrAll;
+    /**
+     * returns the Date value of the requested key
+     *
+     * @param register the key that was requested
+     * @return the date belonging to the key
+     */
+    public LocalDateTime getSolarlogDateField(SOLARLOG300_REGISTER register) {
+        return solarlogDateMap.get(register);
     }
+
 }
--- SolarlogReader/src/test/java/ch/softhenge/solarlog/solarlog/service/SolarlogServiceIntegrationTest.java
+++ SolarlogReader/src/test/java/ch/softhenge/solarlog/solarlog/service/SolarlogServiceIntegrationTest.java
@@ -1,11 +1,9 @@
 package ch.softhenge.solarlog.solarlog.service;
 
 import ch.softhenge.solarlog.solarlog.pojo.Solarlog300Data;
-import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.startsWith;
@@ -26,8 +24,9 @@ public class SolarlogServiceIntegrationTest {
         assertThat(jsonResult, startsWith("{\"801\":{\"170\""));
 
         Solarlog300Data solarlog300Data = new Solarlog300Data(jsonResult);
-        System.out.println("creaDate: " + solarlog300Data.getCreatedDate());
-        System.out.println("pACWrAll: " + solarlog300Data.getPacWrAll());
+        System.out.println("creaDate: " + solarlog300Data.getSolarlogDateField(Solarlog300Data.SOLARLOG300_REGISTER.CREATEDDATE));
+        System.out.println("pACWrAll: " + solarlog300Data.getSolarlogIntegerField(Solarlog300Data.SOLARLOG300_REGISTER.PACWRALL));
+        System.out.println("eACDaySumCnt: " + solarlog300Data.getSolarlogIntegerField(Solarlog300Data.SOLARLOG300_REGISTER.EAC_DAYSUM_CNT));
     }
 
 
